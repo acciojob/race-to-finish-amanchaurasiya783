e7c2352bd68aa79847bad7805af4b600ed7c431a
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-28 21:20:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 window.promises = [];
+function randomPromises(value){
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve(value);
+		}, Math.floor(Math.random*5));
+	})
+}
+promises.push(randomPromises('Promise1'));
+promises.push(randomPromises('Promise2'));
+promises.push(randomPromises('Promise3'));
+promises.push(randomPromises('Promise4'));
+promises.push(randomPromises('Promise5'));
 
+let x = Promise.any(...promises);
+document.getElementById('output').innerHTML = x;
 // Do not change the code above this
 // add your promises to the array `promises`
